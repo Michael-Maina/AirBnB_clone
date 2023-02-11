b58a255709fcedd038c1938cdb73aa696baeb23f
Michael-Maina <michaelmaina02@gmail.com>
2023-02-11 14:34:27 +0300

    Code refactoring on function

--- console.py
+++ console.py
@@ -13,23 +13,17 @@ from models.place import Place
 from models.review import Review
 
 
-def isfloat(arg):
-    """Checks if argument is a float data type variable"""
-    try:
-        float(arg)
-        return True
-    except ValueError:
-        return False
-
-
 def type_parser(arg):
     """Check data type of arg and cast it"""
     if arg.isalpha():
         pass
     elif arg.isdigit():
         arg = int(arg)
-    elif isfloat(arg):
-        arg = float(arg)
+    else:
+        try:
+            arg = float(arg)
+        except ValueError:
+            return False
     return arg
 
 
